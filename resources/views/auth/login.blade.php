665e0c46c04c1e558fef9f0b0088cf141b6ee1d9
Valentin Gautier <valentin.gautier@seqens.fr>
2025-10-22 13:56:46 +0200

    ajout bootstrap

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }}</title>
+    @vite(['resources/scss/app.scss'])
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="mb-1 font-medium text-primary">Let's get started</h1>
+    </div>
+</body>
+
+</html>
